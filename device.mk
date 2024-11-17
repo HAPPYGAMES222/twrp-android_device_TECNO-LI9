ae5758f5e6a0a758c6bb1b5e033f858e2407bd71
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-11-17 04:32:07 -0800

    change and add

--- device.mk
+++ device.mk
@@ -6,7 +6,23 @@
 #
 
 LOCAL_PATH := device/tecno/LI9
+
 # A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    system_ext \
+    vendor \
+    vendor_dlkm \
+    system \
+    boot \
+    vendor_boot \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor \
+    product \
+    odm \
+    odm_dlkm
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
@@ -15,17 +31,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-     android.hardware.boot@1.2-service \
-     android.hardware.boot@1.2-mtkimpl \
-     android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
-     bootctrl \
-     update_engine_client
-
-PRODUCT_PACKAGES += \
-     bootctrl.mt6833 \
-     bootctrl.mt6833.recovery
+     bootctrl 
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -33,10 +43,12 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+	
 # Health HAL
  PRODUCT_PACKAGES += \
      android.hardware.health@2.1-impl \
      android.hardware.health@2.1-service
+	 
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
@@ -59,7 +71,3 @@ PRODUCT_TARGET_VNDK_VERSION := 31
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 31
-
-# Virtual A/B
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
